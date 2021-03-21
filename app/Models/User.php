74789b892fa974db618915fc06fadb5d4e0751c9
jose carlos <jose-carlos.garcia-carranza@epitech.eu>
2021-03-21 02:53:24 +0100

    nre proyect

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+//add class JWTSubject
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject, MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
@@ -40,4 +43,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('testing', function(){
+    return (['user' => 'jose carlos']);
 });
+
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('auth/redirect', function () {
+        return Socialite::driver('google')->redirect();
+    });
+    
+    Route::get('auth/callback', function () {
+        return (['user' => 'jose carlos']);
+        $user = Socialite::driver('google')->user();
+    
+        // $user->token
+    });
+    
+});
+
+//Rutas email verification
+Route::get('email/verify/{id}', 'App\Http\Controllers\VerificationController@verify')->name('verification.verify'); // Make sure to keep this as your route name
+Route::get('email/resend', 'App\Http\Controllers\VerificationController@resend')->name('verification.resend');
+
+
+//Rutas a las que se accederán para registrarse e iniciar sesión
+Route::post('register', 'App\Http\Controllers\UserController@register');
+Route::post('login', 'App\Http\Controllers\UserController@authenticate');
+
+
+
+//Creamos un group para indicar cuales son las rutas que necesitan autenticación:
+Route::group(['middleware' => ['jwt.verify']], function() {
+
+    Route::post('user','App\Http\Controllers\UserController@getAuthenticatedUser');
+
+});
+
