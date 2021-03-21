4c69cdff70d4dcdd77395f263fc4b1dbf2964e81
jose carlos <jose-carlos.garcia-carranza@epitech.eu>
2021-03-21 16:36:51 +0100

    login facebook

--- routes/api.php
+++ routes/api.php
@@ -26,12 +26,12 @@ Route::get('testing', function(){
 
 Route::group(['middleware' => ['web']], function () {
     Route::get('auth/redirect', function () {
-        return Socialite::driver('google')->redirect();
+        return Socialite::driver('facebook')->redirect();
     });
     
     Route::get('auth/callback', function () {
      
-        $user = Socialite::driver('google')->user();
+        $user = Socialite::driver('facebook')->user();
         return ($user);
         // $user->token
     });
