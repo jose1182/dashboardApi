b68921fd16d4453caec08b2e91be554af347547d
jose carlos <jose-carlos.garcia-carranza@epitech.eu>
2021-05-31 15:36:42 +0200

    botoon in home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     <h1>Thanks for your registration!</h1> 
+                    <a href="#" class="badge badge-primary">Home Page</a>
          
                 </div>
 
